f9a7e6e85810592b98dbb758148c1900c03e98ed
lintq <lintq@bbxpc.com>
2019-05-13 14:07:53 +0800

    demo更新

--- test/demo.js
+++ test/demo.js
@@ -4,7 +4,7 @@
 
     QUnit.test('字符串空判断：isNullOrEmpty', function (assert) {
         function empty(str) {
-            if (typeof str == 'undefined' || !str || str.length === 0 || str === "" || !/[^\s]/.test(str) || /^\s*$/.test(str) || str.replace(/\s/g, "") === "") {
+            if (str == 'undefined' || !str || !/[^\s]/.test(str)) {
                 return true;
             } else {
                 return false;
@@ -19,9 +19,18 @@
         }
 
         var student = {className: "测试班", name: "我是张三", age: 18};
-        assert.ok(notEmpty("   "), "非空字符串判断正确");
-        assert.ok(notEmpty(student.skill), "非空字符串判断正确");
-        assert.ok(notEmpty(null), "非空字符串判断正确");
-        assert.ok(notEmpty('undefined'), "非空字符串判断正确");
+        assert.ok(empty("   "), "空字符串判断正确");
+        assert.ok(empty(' '), "空字符串判断正确");
+        assert.ok(empty(student.skill), "空字符串判断正确");
+        assert.ok(empty(undefined), "空字符串判断正确");
+        assert.ok(empty(null), "空字符串判断正确");
+        assert.ok(empty(""), "空字符串判断正确");
+        assert.ok(empty(''), "空字符串判断正确");
+        assert.ok(empty('undefined'), "空字符串判断正确");
+
+        // assert.ok(!notEmpty("   "), "非空字符串判断正确");
+        // assert.ok(notEmpty(student.skill), "非空字符串判断正确");
+        // assert.ok(notEmpty(null), "非空字符串判断正确");
+        // assert.ok(!notEmpty('undefined'), "非空字符串判断正确");
     });
 }());
