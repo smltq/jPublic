ea668a62ac292e7d6ef950e0c73f8b2eebf6ffa2
lintq <lintq@bbxpc.com>
2020-03-23 15:11:25 +0800

    高精度加、减、乘、除函数增加

--- jPublic.js
+++ jPublic.js
@@ -1396,6 +1396,111 @@
     };
 
     /**
+     * 精度加法函数，用来得到精确的加法结果
+     * javascript的加法结果会有误差，在两个浮点数相加的时候会比较明显。这个函数返回较为精确的加法结果。
+     * @param arg1
+     * @param arg2
+     * @returns {number}
+     */
+    _.accAdd = function (arg1, arg2) {
+        var r1, r2, m, c;
+        try {
+            r1 = arg1.toString().split(".")[1].length;
+        } catch (e) {
+            r1 = 0;
+        }
+        try {
+            r2 = arg2.toString().split(".")[1].length;
+        } catch (e) {
+            r2 = 0;
+        }
+        c = Math.abs(r1 - r2);
+        m = Math.pow(10, Math.max(r1, r2));
+        if (c > 0) {
+            var cm = Math.pow(10, c);
+            if (r1 > r2) {
+                arg1 = Number(arg1.toString().replace(".", ""));
+                arg2 = Number(arg2.toString().replace(".", "")) * cm;
+            } else {
+                arg1 = Number(arg1.toString().replace(".", "")) * cm;
+                arg2 = Number(arg2.toString().replace(".", ""));
+            }
+        } else {
+            arg1 = Number(arg1.toString().replace(".", ""));
+            arg2 = Number(arg2.toString().replace(".", ""));
+        }
+        return (arg1 + arg2) / m;
+    };
+
+    /**
+     * 精度减法函数，用来得到精确的减法结果
+     * javascript的减法结果会有误差，在两个浮点数相减的时候会比较明显。这个函数返回较为精确的减法结果。
+     * @param arg1
+     * @param arg2
+     * @returns {string}
+     */
+    _.accSub = function (arg1, arg2) {
+        var r1, r2, m, n;
+        try {
+            r1 = arg1.toString().split(".")[1].length;
+        } catch (e) {
+            r1 = 0;
+        }
+        try {
+            r2 = arg2.toString().split(".")[1].length;
+        } catch (e) {
+            r2 = 0;
+        }
+        m = Math.pow(10, Math.max(r1, r2)); //动态控制精度长度
+        n = (r1 >= r2) ? r1 : r2;
+        return ((arg1 * m - arg2 * m) / m).toFixed(n);
+    };
+
+    /**
+     * 数度乘法函数，用来得到精确的乘法结果
+     * 说明：javascript的乘法结果会有误差，在两个浮点数相乘的时候会比较明显。这个函数返回较为精确的乘法结果。
+     * @param arg1
+     * @param arg2
+     * @returns {number} arg1乘以 arg2的精确结果
+     */
+    _.accMul = function (arg1, arg2) {
+        var m = 0, s1 = arg1.toString(), s2 = arg2.toString();
+        try {
+            m += s1.split(".")[1].length;
+        } catch (e) {
+        }
+        try {
+            m += s2.split(".")[1].length;
+        } catch (e) {
+        }
+        return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
+    };
+
+    /**
+     * 除法函数，用来得到精确的除法结果
+     * 说明：javascript的除法结果会有误差，在两个浮点数相除的时候会比较明显。这个函数返回较为精确的除法结果。
+     * @param arg1
+     * @param arg2
+     * @returns {number} arg1除以arg2的精确结果
+     */
+    _.accDiv = function (arg1, arg2) {
+        var t1 = 0, t2 = 0, r1, r2;
+        try {
+            t1 = arg1.toString().split(".")[1].length;
+        } catch (e) {
+        }
+        try {
+            t2 = arg2.toString().split(".")[1].length;
+        } catch (e) {
+        }
+        with (Math) {
+            r1 = Number(arg1.toString().replace(".", ""));
+            r2 = Number(arg2.toString().replace(".", ""));
+            return (r1 / r2) * pow(10, t2 - t1);
+        }
+    };
+
+    /**
      * 固定小数位数
      * @param {Number}  x       要处理的数值
      * @param {Number}  digits  小数位置，默认保留2位
